c9b1eb7032671e16787844143cf87c636f7080eb
Gabe Cook <gabe@clevyr.com>
2020-12-30 01:29:13 -0600

    :sparkles: Add support for major version tags

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (async () => {
     const core = require('@actions/core');
     const axios = require('axios');
+    const semver = require('semver')
 
     try {
         const now = new Date();
@@ -11,6 +12,13 @@
 
         // Flatten versions into an array
         data = Object.values(data).flatMap((value, key) => Object.keys(value));
+        const maxMajorVersions = data.reduce((acc, version) => {
+            let major = semver.major(semver.coerce(version));
+            if (!(major in acc) || semver.lt(semver.coerce(acc[major]), semver.coerce(version))) {
+                acc[major] = version;
+            }
+            return acc;
+        }, {});
 
         const repo = core.getInput('repo');
         const matrix = {"include": []};
@@ -18,6 +26,9 @@
         // Push entries to matrix
         for (const php_version of data) {
             let extra_tags = '';
+            if (Object.values(maxMajorVersions).includes(php_version)) {
+                extra_tags += `\n${semver.major(semver.coerce(php_version))}`;
+            }
             matrix.include.push({php_version, extra_tags});
         }
 
@@ -26,6 +37,7 @@
 
         core.setOutput('matrix', matrix);
     } catch (error) {
+        console.error(error)
         core.setFailed(error.message);
     }
 })();
